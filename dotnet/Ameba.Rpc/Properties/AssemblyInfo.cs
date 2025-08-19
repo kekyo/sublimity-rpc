28297aaf0fd659c9909b404c09f05d748b5981a5
Kouji Matsui <k@kekyo.net>
2025-08-19 11:02:47 +0900

    feat: Added .NET projects.

---
+++ dotnet/Ameba.Rpc/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿////////////////////////////////////////////////////////////////////////////
+//
+// Ameba pure RPC engine - Simply core implementation of pure RPC engine.
+// Copyright (c) Kouji Matsui (@kekyo@mi.kekyo.net)
+//
+// Licensed under MIT: https://opensource.org/licenses/MIT
+//
+////////////////////////////////////////////////////////////////////////////
+
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Ameba.Rpc.Tests")]
